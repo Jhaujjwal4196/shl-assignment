bdd904a9ce0028f22f55c8ad821fb55bc955241d
jhaujjwal4196 <ujjwal@Ujjwals-MacBook-Air.local>
2023-10-21 21:22:59 +0530

    build fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import { Project } from '@prisma/client';
 import axios from 'axios'
 import React from 'react'
 
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+
 const page = async() => {
   const {data} = await axios.get(`${process.env.SITE_URL}/api/getProjects`);
   
